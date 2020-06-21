ee26b2bf571971cfa6a17c6f9b7be801b587267e
himanihm <himani.mahandru@gmail.com>
2020-06-21 12:41:39 +0530

    message

--- mac/shop/urls.py
+++ mac/shop/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path("tracker/", views.tracker, name="TrackingStatus"),
     path("search/", views.search, name="Search"),
 
-    path("productview/", views.productView, name="ProductView"),
+    path("product/<int:myid>", views.productView, name="ProductView"),
     path("checkout/", views.checkout, name="Checkout"),
 ]
